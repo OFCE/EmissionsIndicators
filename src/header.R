d79fecd1e9edf3453f07cda8f8ffd315ce501984
PaulMalliet <paul.malliet@sciencespo.fr>
2022-06-08 14:50:13 +0200

    Create header.R
    main script to launch to run the computations

---
+++ src/header.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(data.table)
+library(readxl)
+library(openxlsx)
+
+###
+# Choix de la nomenclature: Soit par produit par produit (pxp), soit industrie par industrie (ixi)
+nom <- "pxp"
+
+# Chemin d'accès du dossier où sont stockées les données sources issues d'EXIOBASE 3.8 (fichier txt) (Retrieved from:   https://zenodo.org/record/4277368 )
+path_user <- str_c("C:/Users/PMalliet/")
+path_data.source <- str_c(path_user,"Dropbox (FOSEM)/Databases/EXIOBASE_3.8")
+path_data.source <- str_c("data_in/IOT/")
+
+path_out <- "data_out/"
+
+# Intervalle des années pour extraction des données
+year.min <-2010
+year.max <- 2010
+year <- 2015
+# Choix du pays considéré pour calcul empreinte carbone 
+iso <-  str_c("FR")
+
+# Liste des GES considérés
+glist <- c("CO2", "CH4", "N2O", "SF6", "HFC", "PFC")
