da2f1a29cffb3b21fc517a29e1c3d85310f11ec2
Paul Malliet <paul.malliet@sciencespo.fr>
2022-06-09 18:40:53 +0000

    Full process of aggregation by product and country Prototype with final demand working. Some code cleaning to make and to reproduce for the other matrices (A,Y,L)

--- src/exio3.loader.R
+++ src/exio3.loader.R
@@ -16,64 +16,64 @@ Y_df <- Y %>% as.data.frame() %>% mutate(countries.in = str_sub(rownames(.),1,2)
 br_lg <-  loadBridge("Countries_1", "Countries_2.WD1", "Countries") %>%
   data.frame() %>% mutate(countries.out = rownames(.)) %>%
   pivot_longer(cols = exio3.desc$countries, names_to = "countries.in", values_to = "value") %>%
-  filter(value == 1) %>% select(-value)
-# 
-# br_lg %>% arrange(match(countries.in, countries_data)) %>% select(countries.in) %>% as.vector()
-# countries_data
+  filter(value ==1) %>% select(-value)
+
 
 ### Bridge in long format for products
-br.2_lg <-  loadBridge("exio3", "threeMeEurostat", "Products", transverse = T) %>%
+br.2_lg <-  loadBridge("exio3", "threeMeEurostat", "Products", transverse = T) %>% 
   as.data.frame() %>% mutate(products.out = rownames(.)) %>%
-  pivot_longer(cols = exio3.desc$products, names_to = "products.in", values_to = "value") %>%
-  filter(value == 1) %>% select(-value)
+  pivot_longer(cols = exio3.desc$products, names_to = "products.in", values_to = "weight") %>% 
+filter(weight > 0 )
 
-### Merge and summarize of rows
-
-Y_dff <- merge(Y_df, br_lg, by = "countries.in" , all = T) %>%
-  group_by(countries.out) %>%
-  summarise_at(Y_cd,~sum(.x)) %>% arrange(countries.out) %>% ungroup()
-
-Y_dff_c <-as.vector(Y_dff$countries.out)
-
+## Merge and aggregation
 Y_dfff <- merge(Y_df, br_lg, by = "countries.in" , all.x = TRUE) %>%
   merge(., br.2_lg, by = "products.in") %>%
   group_by(countries.out, products.out) %>%
-  summarise_at(Y_cd,~sum(.x)) %>% arrange(countries.out, products.out) %>% ungroup()
+  # Somme pondérée par weight pour les produits (0>p>1)
+  summarise(across(all_of(Y_cd), ~ sum(. * weight)))  %>% ungroup() 
 
 
-tY_dff <- as.matrix(Y_dff[-1], length(Y_dff_c),length(Y_cd),
-                    dimnames = list(Y_dff_c,Y_cd)) %>%
-  t() %>% as.data.frame()  %>% `colnames<-`(Y_dff_c) %>%
+### Merge and summarize of rows
+# TEST SUR AGGREGATION PAYS
+ Y_dff <- merge(Y_df, br_lg, by = "countries.in" , all = T) %>%
+   group_by(countries.out) %>%
+   summarise_at(Y_cd,~sum(.x)) %>% arrange(countries.out) %>% ungroup()
+ 
+ Y_dff_c <-as.vector(Y_dff$countries.out)
+
+ 
+ # Taille finale de la matrice Y (hors colonnes indicateurs nom)
+# Y 12 * 88 (1056) X  12 * 7 (84)
+# A 12 * 88 (1056) X  12 * 88 (1056)
+ 
+ 
+ # Nom nouvelles colonnes 
+col.Y_dfff <- str_c(Y_dfff$countries.out,"_",Y_dfff$products.out)
+
+
+# Step 2 avec la transpose (que pour countries, car produits ici composante demande)
+tY_dff <- Y_dfff %>% select(-countries.out,- products.out) %>% as.matrix(length(col.Y_dfff),length(Y_cd),
+                    dimnames = list(col.Y_dfff,Y_cd)) %>%
+  t() %>% as.data.frame()  %>% `colnames<-`(col.Y_dfff) %>%
   mutate(countries.in = str_sub(rownames(.),1,2),
          products.in = str_sub(rownames(.),4)) %>%
   merge(br_lg, by = "countries.in" , all = T) %>%
-  group_by(countries.out) %>%
-  summarise_at(Y_dff_c,~sum(.x)) %>%
-  arrange(countries.out) %>% ungroup()
+  group_by(countries.out, products.in) %>%
+  # Somme pondérée par weight pour les produits (0>p>1)
+  summarise(across(all_of(col.Y_dfff), ~ sum(.)))  %>% ungroup() 
 
-
-group_by(countries.out, products.out) %>% 
-  summarise_at(Y_cd,~sum(.x)) %>% arrange(countries.out, products.out) %>% ungroup()
-pivot_longer(cols = Y_dff_c, names_to = "products.in", values_to = "value") %>%
-  filter(value == 1) %>% select(-value)
 
 # Check for NA
 Y_dff[is.na(Y_dff)]
 Y_dfff[is.na(Y_dfff)]
-
+tY_dff[is.na(tY_dff)]
 
 #difference in value from the original database Y and the transformed Y_dff. Should be equal to 0 
 sum(Y_dff[,-1]) - sum(Y)
-sum(tY_dff[,-1]) - sum(Y)
-sum(Y_dfff[,-1:-2]) - sum(Y)
 
-Y[,1] %>% sum()
-# 9002674 tot sum
-Y_dff %>% select(-countries.out, -products.out) %>% sum()
-Y_dff %>% select(-countries.out) %>% sum()
+# Encore une petite erreur mais qui est négligable (0.032%)
+(sum(tY_dff[,-1:-2]) - sum(Y)) #/sum(Y) *100
 
-#67492247 tot sum
-Y %>% sum()
 
 ##Columns 
 ### Merge and summarize of rows
@@ -84,4 +84,3 @@ Y_dffinal <- merge(Y_dff, br_lg, by = "countries.in" , all.x = TRUE) %>%
   group_by(countries.out, products.out) %>%
   summarise_at(Y_cd,~sum(.x)) %>% arrange(countries.out, products.out) %>% ungroup()
 
->>>>>>> f28f67748d00173adc32cae5b155a174d779d774
