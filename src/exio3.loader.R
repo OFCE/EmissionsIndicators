4b46a24ee9d9bcd51b7446036a28c357ace68d66
LeaSettepani <leasylverlesc@gmail.com>
2022-06-10 16:15:35 +0200

    comments

--- src/exio3.loader.R
+++ src/exio3.loader.R
@@ -7,7 +7,7 @@ A <- readRDS(str_c(path_codedata,"data.out/IOT_",year,"_",nom,"/A.rds"))
 Y <- readRDS(str_c(path_codedata,"data.out/IOT_",year,"_",nom,"/Y.rds"))
 Fe <- readRDS(str_c(path_codedata,"data.out/IOT_",year,"_",nom,"/F.rds"))
 
-#? Y_d <- readRDS(str_c(path_codedata,"data.out/IOT_",year,"_",nom,"/Y_types_DF.rds"))
+Y_d <- readRDS(str_c(path_codedata,"data.out/IOT_",year,"_",nom,"/Y_types_DF.rds"))
 A_cd <- readRDS(str_c(path_codedata,"data.out/IOT_",year,"_",nom,"/A_pays_secteurs.rds"))
 Y_cd <- readRDS(str_c(path_codedata,"data.out/IOT_",year,"_",nom,"/Y_pays_types_DF.rds"))
 Y_c <- readRDS(str_c(path_codedata,"data.out/IOT_",year,"_",nom,"/Y_pays.rds"))
@@ -91,20 +91,23 @@ View(tY_dfff)
 ####Mêmes étapes pour A
 A_df <- A %>% as.data.frame() %>% mutate(countries.in = str_sub(rownames(.),1,2),
                                          products.in = str_sub(rownames(.),4))
+
+## Merge and aggregation
 A_dfff <- merge(A_df, br_lg, by = "countries.in" , all.x = TRUE) %>%
   merge(., br.2_lg, by = "products.in") %>%
   group_by(countries.out, products.out) %>%
   # Somme pondérée par weight pour les produits (0>p>1)
   summarise(across(all_of(A_cd), ~ sum(. * weight)))  %>% ungroup()
 
-A_dff <- merge(A_df, br_lg, by = "countries.in" , all = T) %>%
-  group_by(countries.out) %>%
-  summarise_at(A_cd,~sum(.x)) %>% arrange(countries.out) %>% ungroup()
+#A_dff <- merge(A_df, br_lg, by = "countries.in" , all = T) %>%
+#  group_by(countries.out) %>%
+#  summarise_at(A_cd,~sum(.x)) %>% arrange(countries.out) %>% ungroup()
 
-A_dff_c <-as.vector(A_dff$countries.out)
+#A_dff_c <-as.vector(A_dff$countries.out)
 
 col.A_dfff <- str_c(A_dfff$countries.out,"_",A_dfff$products.out)
 
+# Step 2 avec la transpose
 tA_dff <- A_dfff %>% select(-countries.out,- products.out) %>% 
   as.matrix(length(col.A_dfff),length(A_cd),
             dimnames = list(col.A_dfff,A_cd)) %>%
@@ -118,13 +121,16 @@ tA_dff <- A_dfff %>% select(-countries.out,- products.out) %>%
   # Somme pondérée par weight pour les produits (0>p>1)
   summarise(across(all_of(col.A_dfff), ~ sum(.)))  %>% ungroup()
 
-A_dff[is.na(A_dff)]
+# Check for NA
+#A_dff[is.na(A_dff)]
 A_dfff[is.na(A_dfff)]
 tA_dff[is.na(tA_dff)]
 
+#difference in value from the original database Y and the transformed Y_dff. Should be equal to 0 
 sum(A_dff[,-1]) - sum(A)
 (sum(tA_dff[,-1:-2]) - sum(A)) /sum(A) *100
 
+#Cleaning (noms des lignes et colonnes,...)
 tA_dfff <- tA_dff %>% select(-countries.out,- products.out) %>% 
   as.matrix(length(nrow(tA_dff)),length(ncol(tA_dff)),
             dimnames = list(col.A_dfff,col.A_dfff)) %>%
@@ -133,23 +139,30 @@ View(tA_dfff)
 
 (sum(tA_dfff[,-1:-2]) - sum(A)) /sum(A) *100 #Erreur trop importante: 8,67%
 
-####Mêmes étapes pour Fe
+####Mêmes étapes pour Fe (plus court)
+
 F_df <- Fe %>% as.data.frame() %>% t() %>% as.data.frame() %>% mutate(countries.in = str_sub(rownames(.),1,2),
                                          products.in = str_sub(rownames(.),4))
+
+## Merge and aggregation
 F_dfff <- merge(F_df, br_lg, by = "countries.in" , all.x = TRUE) %>%
   merge(., br.2_lg, by = "products.in") %>%
   group_by(countries.out, products.out) %>%
   # Somme pondérée par weight pour les produits (0>p>1)
   summarise(across(all_of(F_noms_extensions), ~ sum(. * weight)))  %>% ungroup()
 
+# Check for NA
 F_dfff[is.na(F_dfff)]
 tF_dfff[is.na(tF_dfff)]
 
+#Cleaning (noms des lignes et colonnes,...)
 row.F_dfff <- str_c(F_dfff$countries.out,"_",F_dfff$products.out)
 tF_dfff <- F_dfff %>% select(-countries.out,- products.out) %>% 
   as.matrix(length(row.F_dfff),length(F_noms_extensions),
             dimnames = list(row.F_dfff,F_noms_extensions)) %>%
   t() %>% as.data.frame()  %>% `colnames<-`(row.F_dfff)
 View(tF_dfff)
+
+#difference in value from the original database Y and the transformed Y_dff. Should be equal to 0 
 (sum(tF_dfff[,-1:-2]) - sum(Fe))  /sum(Fe) *100 #Erreur de 0.95%
 
