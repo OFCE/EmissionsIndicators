31806761e278c74877a36f6d055573f876e3162e
LeaSettepani <leasylverlesc@gmail.com>
2022-06-13 18:21:26 +0200

    tableau 1 pays

--- src/IO.analysis.R
+++ src/IO.analysis.R
@@ -2,7 +2,7 @@ br <- "ThreeMe"
 
 # Chargement des données I-O sauvegardées par le script exio3.Pre-loader.R
 A <-readRDS(str_c("data_out/IOT_",year,"_",nom,"/A_",br,".rds"))
-Y <-readRDS(str_c("data_out/IOT_",year,"_",nom,"/Y_",br,".rds"))
+Y <-readRDS(str_c(path_out,"/Y_",br,".rds"))
 Fe <-readRDS(str_c("data_out/IOT_",year,"_",nom,"/Fe_",br,".rds"))
 
 ## Calcul de L: matrice de Leontief
@@ -21,6 +21,7 @@ rm(Y)
 
 ## calcul de S
 # x (production totale (pour CI + pour DF))
+#filtrer L ici pour avoir l'indicateur pour un pays donné?
 x <- (L %*% y_tot) %>% as.numeric()
 
 
@@ -79,9 +80,9 @@ GES_list[["GES"]] <- GES_list[["CO2"]] +
   GES_list[["CH4"]] +
   GES_list[["N2O"]] +
   GES_list[["SF6"]] +
-  GES_list[["HFC"]] +
+  GES_list[["HFC"]] + #déjà convertis
   GES_list[["PFC"]]
-
+#Crée un vecteur (1065x1) (de quantité de GES émise par chaque secteur ?)
 
 print("Computation of the environemental impact (S) : done")  
 
@@ -103,13 +104,54 @@ print("Computation of the environmental impact multipliers (M) : done")
 
 
 ##saveRDS( M, str_c("EXIOBASE/data/IOT_",year,"_",nom,"/M.rds"))
-#saveRDS( M.alt, str_c("EXIOBASE/data/IOT_",year,"_",nom,"/M.alt.rds"))
-#saveRDS( M.dir, str_c("EXIOBASE/data/IOT_",year,"_",nom,"/M.dir.rds"))
-#saveRDS( M.ndir, str_c("EXIOBASE/data/IOT_",year,"_",nom,"/M.ndir.rds"))
-print(str_c("Les matrices S, L, M et M.dir ainsi que le vecteur x ont ete calculees et enregistrees a l'adresse suivante~:EXIOBASE/data/IOT_",year,"_",nom,"/"))
-#où est M.dir ?
+print(str_c("Les matrices S, L et M ainsi que le vecteur x ont ete calculees et enregistrees a l'adresse suivante~:EXIOBASE/data/IOT_",year,"_",nom,"/"))
 
 
-rm(list=c("L", "L.s1","S","x"))
-#où est L.s1
+rm(list=c("L","S","x"))
 rm(GES_list)
+
+#Obtenir un dataframe avec les données pour un pays
+###exemple pour un seul pays/région
+DF_france = Y
+DF_france[-str_which(rownames(DF_france),"France"),]<-0
+DF_tot_to_france <- as.matrix(DF_france)
+DF_tot_to_france <- DF_tot_to_france %*% Id(DF_tot_to_france)
+
+L_france = L
+L_france[,-str_which(colnames(L),"France")]<-0
+#prochain bloc = pour que L (matrice) "rentre dans une ligne": le total 
+#des CI nécessaires à la France pour produire chaque produit
+CI_of_france <- as.matrix(L_france) %>% t()
+CI_of_france <- CI_of_france %*% Id(CI_of_france)
+
+x_france <- (L_france %*% y_tot) %>% as.numeric
+
+#le reste=A VERIFIER
+x_1_france <- 1/x_france
+x_1_france[is.infinite(x_1_france)] <- 0 
+x_1_france <- as.numeric(x_1_france)
+x_1d_france <- diag(x_1_france)
+coef_envir_france <- as.matrix(Fe) %*% x_1d_france %>% t()
+coef_envir_france <- coef_envir_france %*% Id(coef_envir_france)
+
+impact_production_france <- coef_envir_france %*% L_france %>% t()
+impact_production_france <- impact_production_france %*% Id(impact_production_france)
+
+table = data.frame(
+  impact_production_france,x_france,DF_tot_to_france,CI_of_france,coef_envir_france
+)
+
+###généralisation
+TableFinale<-function(pays){
+  for (pays in regions) { 
+    pays_select=
+    M_select=
+    x_select=
+    DF_select=
+    L_select=
+    S_select=
+    table = data.frame(
+      pays_select,M_select,x_select,DF_select,L_select,S_select
+      )
+  }
+}
