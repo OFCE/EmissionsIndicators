0ada8471579f247237167809b05805e74bd6bc11
LeaSettepani <leasylverlesc@gmail.com>
2022-06-15 12:58:39 +0200

    Update IO.analysis.R

--- src/IO.analysis.R
+++ src/IO.analysis.R
@@ -111,31 +111,42 @@ rm(list=c("L","S","x"))
 rm(GES_list)
 
 
-#Obtenir un dataframe avec les données pour un pays
+#Obtenir un dataframe avec les données pour un pays:
 ###Généralisation pour n'importe quel pays
 
 #Chemin pour exporter
 #dir.create(str_c(path_out, "/IO_pays"), recursive = TRUE)
 #path_IOpays_tables <- str_c(path_out, "/IO_pays")
 
+#Boucle qui crée un tableau avec les indicateurs pour chaque pays
+#(il faut avoir Y et L au préalable)
 for (pays in c("France","EU","US","Chine","Amerique du N.","Amerique du S.","Afrique","Russie","Europe (autres)","Asie","Moyen-Orient","Oceanie" )) {
+  
+  #Colonne nom pays (pas nécessaire si pas rbind par la suite)
+  nom_pays <- c(rep(pays,1056))
   
   #Colonne demande finale
   DF=Y
+  #mettre à 0 les entrées des autres pays (demande finale adressée au pays en question)
   DF[-str_which(rownames(DF),as.character(pays)),]<-0
-  DF_tot <- as.matrix(DF) %*% Id(DF)
+  DF_tot <- as.matrix(DF) %*% Id(DF) #somme de toutes les composantes
+  #interprétation : 
   
-  #Colonne production
+  #Vecteur production (identique au vecteur monde)
   production <- (L %*% DF_tot) %>% as.numeric
+  #interprétation :
   
-  #Matrice S (impact producteur)
+  #Matrice S ("impact producteur") : impact environnemental (uniquement demande pays)
   x_1_select <- 1/production
   x_1_select[is.infinite(x_1_select)] <- 0 
   x_1_select <- as.numeric(x_1_select)
   x_1d_select <- diag(x_1_select)
   S_select <- as.matrix(Fe) %*% x_1d_select
+  #interprétation
   
+  #Convertir en CO2 équivalent (éliminer les autres impacts)
   GES_list_select <- list()
+  ##Sélectionner les impacts
   GES_list_select[["GES.raw"]] <- S_select %>% as.data.frame %>% 
     filter(str_detect(row.names(.), "CO2") |
           str_detect(row.names(.), "CH4") | 
@@ -143,27 +154,29 @@ for (pays in c("France","EU","US","Chine","Amerique du N.","Amerique du S.","Afr
           str_detect(row.names(.), "SF6") | 
           str_detect(row.names(.), "PFC") | 
           str_detect(row.names(.), "HFC") )
+  ##Faire la conversion
   for (ges in glist){
     #Row number for each GES in the S matrix
     id_row <- str_which(row.names(GES_list_select[["GES.raw"]]),str_c(ges))
     GES_list_select[[str_c(ges)]] <- GES_list_select[["GES.raw"]][id_row,] %>% colSums() %>% as.data.frame()
+    GES_list_select[[ges]] <- GHGToCO2eq(GES_list_select[[ges]])
   }
-  for (ges in c("CH4","N2O","SF6")){GES_list_select[[ges]] <- GHGToCO2eq(GES_list_select[[ges]])}
+  ##Total par type de gaz (toute source confondue)
   GES_list_select[["GES"]] <- GES_list_select[["CO2"]] +
     GES_list_select[["CH4"]] +
     GES_list_select[["N2O"]] +
     GES_list_select[["SF6"]] +
-    GES_list_select[["HFC"]] + #déjà convertis
+    GES_list_select[["HFC"]] +
     GES_list_select[["PFC"]]
+  #Colonne impact GES
   GES_impact_prod <- GES_list_select[["GES"]]
+  #interprétation :
   
-  #Matrice M (impact demande finale)
+  #Vecteur équivalent à M ("impact demande finale" adressée au pays)
   GES_impact_DF <- as.matrix(GES_impact_prod %>% t()) %*% L %>% t()
+  #interprétation : 
   
-  #pas utile si pas rbind par la suite
-  nom_pays <- c(rep(pays,1056))
-  
-  #créer le tableau
+  #Créer le tableau en assemblant les colonnes
   GES_impact_producteur=as.numeric(unlist(GES_impact_prod))
   GES_impact_demande=as.numeric(unlist(GES_impact_DF))
   assign("io_table",
@@ -172,7 +185,7 @@ for (pays in c("France","EU","US","Chine","Amerique du N.","Amerique du S.","Afr
            )
          )
   
-  #mettre à 0 production pour les autres pays
+  #Mettre à 0 la production pour les autres pays (tableau spécifique à un seul pays)
   io_table[-str_which(rownames(io_table),as.character(pays)),5]<-0
   
   #Créer une colonne produits, ordonner les colonnes
@@ -180,7 +193,7 @@ for (pays in c("France","EU","US","Chine","Amerique du N.","Amerique du S.","Afr
   io_table$produits=sub(".*?_", "",io_table$produits)
   io_table = io_table %>% select(nom_pays,produits,DF_tot,production,GES_impact_producteur,GES_impact_demande)
   
-  #exporter
+  #Exporter le tableau
   saveRDS(io_table, str_c(path_IOpays_tables, "/IO_", pays, ".rds"))
 }
 
