450b46588467ee33683cb755049049ce97582634
Paul Malliet <paul.malliet@sciencespo.fr>
2022-06-24 22:59:02 +0000

    Addendum to satellite account and format data of data

--- src/functions/02_perform.bridge.R
+++ src/functions/02_perform.bridge.R
@@ -2,7 +2,9 @@ perform.bridge <- function(data,
                            country_in, country_out, country_sht, 
                            sec_in, sec_out, sec_sht, 
                            sq_mat = NULL, 
-                           format_data = NULL)
+                           format_data = NULL, 
+                           transpose = NULL, 
+                           satellite = NULL)
 {
   
   
@@ -10,11 +12,26 @@ perform.bridge <- function(data,
   
   if(is.null(format_data)){format_data = "data.frame"}
   
+  if(is.null(transpose)){transpose = FALSE}
+  
+  if(is.null(satellite)){satellite = FALSE}
+  
   
   df <- data %>% as.data.frame() %>% mutate(countries.in = str_sub(rownames(.),1,2),
                                             products.in = str_sub(rownames(.),4))
+
+  if (transpose == TRUE){
+ 
+  df_0 <-   t(data)
+     df <- df_0%>% as.data.frame() %>% mutate(countries.in = str_sub(rownames(.),1,2),
+                                            products.in = str_sub(rownames(.),4))
   
-  id.in = str_c(df$countries.in,"_",df$products.in)
+  } else { 
+    df_0 <- data
+    df <- df_0 %>% as.data.frame() %>% mutate(countries.in = str_sub(rownames(.),1,2),
+                                              products.in = str_sub(rownames(.),4))
+  }
+  
   
   br_lg <-  loadBridge(country_in, country_out, country_sht) %>%
     data.frame() %>% mutate(countries.out = rownames(.)) %>%
@@ -30,19 +47,29 @@ perform.bridge <- function(data,
   
   
   ## Merge and aggregation
+  if (satellite == TRUE){
   df.1 <- merge(df, br_lg, by = "countries.in" , all.x = TRUE) %>%
     merge(., br.2_lg, by = "products.in") %>%
     group_by(countries.out, products.out) %>%
     # Somme pondérée par weight pour les produits (0>p>1)
-    summarise(across(all_of(colnames(data)), ~ sum(. * weight)))  %>% ungroup() 
+    summarise(across(all_of(colnames(df_0)), ~ sum(. * weight)))  %>% ungroup() %>%
+    rename_with(~ sub("^countries.*", "countries", .x), starts_with("countries")) %>%
+    rename_with(~ sub("^products.*", "products", .x), starts_with("products"))
   
+  id_out = str_c(df.1$countries,"_",df.1$products)
   
-  id_out = str_c(df.1$countries.out,"_",df.1$products.out)
-  
-  
-  
+  } else {
+
+    df.1 <- merge(df, br_lg, by = "countries.in" , all.x = TRUE) %>%
+      merge(., br.2_lg, by = "products.in") %>%
+      group_by(countries.out, products.out) %>%
+      # Somme pondérée par weight pour les produits (0>p>1)
+      summarise(across(all_of(colnames(df_0)), ~ sum(. * weight)))  %>% ungroup() 
+    
+    id_out = str_c(df.1$countries.out,"_",df.1$products.out)
+    
   # Step 2 avec la transpose (que pour countries, car produits ici composante demande)
-  df.2 <- df.1 %>% select(-countries.out,- products.out) %>%
+  df.1 <- df.1 %>% select(-countries.out,- products.out) %>%
     as.matrix(nrow(df.1),ncol(data),
               dimnames = list(id_out,data)) %>%
     t() %>% as.data.frame() %>% `colnames<-`(id_out) %>%
@@ -53,47 +80,54 @@ perform.bridge <- function(data,
     # Somme pondérée par weight pour les produits (0>p>1)
     summarise(across(all_of(id_out), ~ sum(.)))  %>% ungroup() 
   
-  
-  
-  
+ 
   
   if (sq_mat == TRUE){
     
-    df.2 <- df.2 %>% 
+    df.1 <- df.1 %>% 
       merge(br.2_lg, by = "products.in", all = T) %>%
       group_by(countries.out, products.out) %>%
       # Somme pondérée par weight pour les produits (0>p>1)
-      summarise(across(all_of(id.out), ~ sum(.)))  %>% ungroup() %>%
-      rename(countries = "countries.out", 
-             products = "products.out")
+      summarise(across(all_of(id_out), ~ sum(.)))  %>% ungroup() %>%
+      rename_with(~ sub("^countries.*", "countries", .x), starts_with("countries")) %>%
+      rename_with(~ sub("^products.*", "products", .x), starts_with("products"))
+      #rename(countries = "countries.out", 
+      #       products = "products.out")
     
-    id_out.col <- str_c(df.2$countries,"_",df.2$products)
-    id_out.row <- colnames(select(df.2, - products, -countries))
     
-  } else { df.2 <- df.2 %>%
-    rename(countries = "countries.out", 
-           products = "products.in")}
+    id_out.col <- str_c(df.1$countries,"_",df.1$products)
+    id_out.row <- colnames(select(df.1, - products, -countries))
+    
+  } 
+  else { 
+    df.1 <- df.1 %>% 
+      rename_with(~ sub("^countries.*", "countries", .x), starts_with("countries")) %>%
+      rename_with(~ sub("^products.*", "products", .x), starts_with("products"))
+    
+    
+
+    }
   
-  id_out.col <- str_c(df.2$countries,"_",df.2$products)
-  id_out.row <- colnames(select(df.2, - products, -countries))
-  
+  id_out.col <- str_c(df.1$countries,"_",df.1$products)
+  id_out.row <- colnames(select(df.1, - products, -countries))
+  }
   
   #Choice of the format of export of the datatable (either data.frame or matrix)
   if (format_data == "matrix"){
     
-    df.2 <- df.2 %>% select(-countries, - products) %>% 
+    df.1 <- df.1 %>% select(-countries, - products) %>% 
       as.matrix(nrow(.),ncol(.),
                 dimnames = list(id_out.row,id_out.col))
     
     
     
-  } else{ 
+  } #else{ 
     # Transpose and datawrangling
-    df.2 <- df.2 %>% select(all_of(id_out)) %>% 
-      t() %>% as.data.frame() %>% `colnames<-`(id_out.col) %>% 
-      mutate(countries = str_extract(rownames(.),"^.+?(?=_)"),
-             products = str_extract(rownames(.),"(?<=_)(.*)")) %>% 
-      select(countries, products, all_of(id_out.col))
-  }
-  df.2
+    # df.1 <- df.1 %>% select(all_of(id_out)) %>% 
+    #   t() %>% as.data.frame() %>% `colnames<-`(id_out.col) %>% 
+    #   mutate(countries = str_extract(rownames(.),"^.+?(?=_)"),
+    #          products = str_extract(rownames(.),"(?<=_)(.*)")) %>% 
+    #   select(countries, products, all_of(id_out.col))
+  #}
+  df.1
 }
