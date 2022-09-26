2065081fdb03e8e6e4062e4bffac5a40fce185a9
Paul Malliet <paul.malliet@sciencespo.fr>
2022-09-26 14:14:47 +0000

    tiny corrections: ambiguous command

--- src/functions/01_load.bridge.R
+++ src/functions/01_load.bridge.R
@@ -8,19 +8,19 @@ loadBridge <- function(from, to, sht, transverse = NULL) {
                  str_c("data_in/bridges/", from, ".to.", to, ".xlsx"))
   
   # Industry names
-  row.names <- openxlsx::read.xlsx(path, sheet = sht, cols = 1, colNames = F) %>% .[[1]] 
+  row.names <- openxlsx::read.xlsx(path, sheet = sht, cols = 1, colNames = FALSE) %>% .[[1]] 
   # Keep only non-NA cells
   row.count <- sum(!is.na(row.names))
   #row.names <- row.names[2:(1 + row.count)]
   
   # Product names
-  col.names <- openxlsx::read.xlsx(path, sheet = sht, rows = 1, colNames = F) %>% t %>% as.vector
+  col.names <- openxlsx::read.xlsx(path, sheet = sht, rows = 1, colNames = FALSE) %>% t %>% as.vector
   # Keep only non-NA cells
   col.count <- sum(!is.na(col.names))
   #col.names <- col.names[2:(1 + col.count)]
   
   # Correspondence matrix
-  mat <- openxlsx::read.xlsx(path, sheet = sht, rows = c(2:(1+length(row.names))),cols = c(2:(1+length(col.names))), colNames = F)  %>% as.matrix
+  mat <- openxlsx::read.xlsx(path, sheet = sht, rows = c(2:(1+length(row.names))),cols = c(2:(1+length(col.names))), colNames = FALSE)  %>% as.matrix
   rownames(mat) <- row.names
   colnames(mat) <- col.names
   mat[is.na(mat)] <- 0
