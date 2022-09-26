0d1c1dee556b8dc19d2d1953f1a60cd03e09b969
Paul Malliet <paul.malliet@sciencespo.fr>
2022-09-26 14:23:18 +0000

    lil update

--- src/00_header.R
+++ src/00_header.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(data.table)
 library(openxlsx)
-library("zen4R")
+library(zen4R)
 
 ###
 # Choix de la nomenclature: Soit par produit par produit (pxp), soit industrie par industrie (ixi)
@@ -33,19 +33,6 @@ path_loader <- str_c(path_out, br, "/",br.pays,"/")
 path_ResultsTable <- str_c(path_user,"results/IO_pays/",year,"/",br.pays,"_",br)
 path_ResultsPlot <- str_c(path_user,"results/plots/")
 
-### Creation of the subfolders
-# Folder where data from the IO calcul are stored in a folder with the name of the nomenclature used. File Indicated by year (y) and country (iso) such that. (ex: data_out_2015_FR)
-dir.create(str_c(path_ResultsTable), recursive = TRUE)
-
-# Folder to store the source code associated to output plots and tables
-dir.create(str_c("src/plots"), recursive = TRUE)
-dir.create(str_c("src/tables"), recursive = TRUE)
-
-# Folder to store the image and code associated to src plots and tables
-dir.create(str_c("results/plots"), recursive = TRUE)
-dir.create(str_c("results/tables"), recursive = TRUE)
-
-
 
 # Descriptions of nomenclatures
 source("data_in/desc/exio3.desc.R")
@@ -60,3 +47,15 @@ source("src/functions/02_perform.bridge.R")
 
 sec.desc <- list("exio3.desc" = exio3.desc,
                  "ThreeME.desc" = ThreeME.desc)
+
+### Creation of the subfolders
+# Folder where data from the IO calcul are stored in a folder with the name of the nomenclature used. File Indicated by year (y) and country (iso) such that. (ex: data_out_2015_FR)
+dir.create(str_c(path_ResultsTable), recursive = TRUE)
+
+# Folder to store the source code associated to output plots and tables
+dir.create(str_c("src/plots"), recursive = TRUE)
+dir.create(str_c("src/tables"), recursive = TRUE)
+
+# Folder to store the image and code associated to src plots and tables
+dir.create(str_c("results/plots"), recursive = TRUE)
+dir.create(str_c("results/tables"), recursive = TRUE)
