28ea6395514704472b576ccb5fdd637876770419
PaulMalliet <paul.malliet@sciencespo.fr>
2022-09-26 23:18:07 +0200

    Update 04_export.ThreeME.R
    Update with add of inputs for excel tables (volumes and emissions factor for indirect and scope 1 emissions)

--- src/04_export.ThreeME.R
+++ src/04_export.ThreeME.R
@@ -13,34 +13,92 @@ year <- 2015
 
 brd <- "ThreeME"
 ghg <- 'CO2'
-# Creation of an excel workbook
+
+
+table.import <- function(data){
+  df <- (data)  %>% group_by("countries","code", "products")  %>%
+    summarise(across(all_of(colnames(data[,-1:-3])), ~ sum(.)))  %>% ungroup() %>%
+    .[,-1:-3] %>% 
+    t %>% as.data.frame() %>% 
+    mutate(countries = str_sub(rownames(.),1,2),
+           products = str_sub(rownames(.),4)) %>%
+    pivot_wider(names_from = countries,
+                values_from = V1)  %>% select(-iso)
+  return(df)
+}
+
 wb <- createWorkbook("ThreeME calibration")
+wb_dir <- createWorkbook("ThreeME calibration.2")
 
   # Adding the exports in monetary values into the workbook with each sheet per type of ghg
-  
-  #for (ghg in c(glist, "GES")){
-    
 
-
+  for (ghg in c(glist)){
+ 
     #Extracting the data and taking only the imported emissions 
-    data_EMS.M <- readRDS(str_c(path_loader,"M_",iso,"_",ghg,".rds"))  %>%
-      select(-iso) %>% merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
+    data_fac.M <- readRDS(str_c(path_loader,"fac.M_",ghg,"_",iso,".rds"))  %>%
+      merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
       select(-nProducts)
 
-    #Extracting the data and taking only the imported emissions for intermediate use
-    data_EMS.CIM <- readRDS(str_c(path_loader,"M_",iso,"_",ghg,".rds"))  %>%
-      select(-iso) %>% merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
+    X  <- readRDS(str_c(path_loader,"X_",br.pays,"_",br,".rds"))  %>% as.data.frame() %>%
+      mutate(countries = str_sub(rownames(.),1,2),
+             products = str_sub(rownames(.),4)) %>%
+      pivot_wider(names_from = countries,
+                  values_from = indout_0) %>% select(-iso)
+    
+    
+    EMS.M <- readRDS(str_c(path_loader,"M_",ghg,"_",iso,".rds"))  %>%
+      merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
       select(-nProducts)
     
+    EMS.M_dir <- readRDS(str_c(path_loader,"M_dir_",ghg,"_",iso,".rds"))  %>%
+      merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
+      select(-nProducts)
+    
+   # EMS.CIM <- readRDS(str_c(path_loader,"CIM_",ghg,"_",iso,".rds"))  %>%
+  #    merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
+  #    select(-nProducts)
+
+    #EMS.tot <- (EMS_M.mat) %>% `rownames<-`(colnames(data_EMS.M))
+    
+    
+    # Emissions par produit et par pays d'origine
+    
+    data_EMS.M <- table.import(EMS.M)  %>% merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
+      select(-nProducts)
+    data_EMS.M_dir <- table.import(EMS.M_dir)  %>% merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
+      select(-nProducts)
+    
+    # Intensity by products
+    data_IEMS_M <- cbind("products" =data_EMS.M[,1], data_EMS.M[,-1:-2]/X[,-1]) %>% merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
+      select(-nProducts)
+    data_IEMS_M[is.infinite(data_IEMS_M)] <- 0 
+    data_IEMS_M[is.na(data_IEMS_M)] <- 0 
+  
+    data_IEMS_M_dir <- cbind("products" = data_EMS.M_dir[,1], data_EMS.M_dir[,-1:-2]/X[,-1])%>% merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
+      select(-nProducts)
+    data_IEMS_M_dir[is.infinite(data_IEMS_M_dir)] <- 0 
+    data_IEMS_M_dir[is.na(data_IEMS_M_dir)] <- 0 
+    
+    
+    # Creation of an excel workbook
+   
     # Writing the data into a excel workbook sheet 
     addWorksheet(wb, str_c(ghg))
     writeData(wb, str_c(ghg), get(str_c("data_EMS.M")), rowNames = TRUE)
-    addWorksheet(wb, str_c("CIM_",ghg))
-    writeData(wb, str_c("CIM_",ghg), get(str_c("data_EMS.CIM")), rowNames = TRUE)
+    addWorksheet(wb, str_c("fac_",ghg))
+    writeData(wb, str_c("fac_",ghg), data_IEMS_M, rowNames = TRUE)
     
-  #}
+   
+    # Writing the data into a excel workbook sheet 
+    addWorksheet(wb_dir, str_c(ghg))
+    writeData(wb_dir, str_c(ghg), get(str_c("data_EMS.M_dir")), rowNames = TRUE)
+    addWorksheet(wb_dir, str_c("fac_",ghg))
+    writeData(wb_dir, str_c("fac_",ghg), get(str_c("data_IEMS_M_dir")), rowNames = TRUE)
+}
+    
+    
   # Adding the exports in monetary values
-  data_M <- readRDS(str_c(path_loader,"Imports.Y_",iso,".rds"))  %>%
+  data_M <- readRDS(str_c(path_loader,"Y_",iso,".rds"))  %>%
     select(-iso) %>% merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
     select(-nProducts)
   
@@ -48,44 +106,10 @@ wb <- createWorkbook("ThreeME calibration")
   addWorksheet(wb, "IMPORTS")
   writeData(wb, "IMPORTS", data_M, rowNames = TRUE)
   
+  addWorksheet(wb_dir, "IMPORTS")
+  writeData(wb_dir, "IMPORTS", data_M, rowNames = TRUE)
+  
 
   # Exporting the results in the path_out folder
   saveWorkbook(wb, file = str_c(path_user,path_export,"DATA_GHG_IMPORTED.xlsx"), overwrite = TRUE)
-
-  
-  # Creation of an excel workbook
-  wb <- createWorkbook("ThreeME calibration")
-  
-  # Adding the exports in monetary values into the workbook with each sheet per type of ghg
-  
-  for (ghg in c(glist, "GES")){
-  
-  #Extracting the data and taking only the imported emissions 
-  
-  #Extracting the data and taking only the imported emissions 
-  data_EMS.CIM <- readRDS(str_c(path_loader,"S_",iso,"_",ghg,".rds"))  %>%
-    select(-iso) %>% merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
-    select(-nProducts)
-  
-  # Writing the data into a excel workbook sheet 
-  addWorksheet(wb, str_c(ghg))
-  writeData(wb, str_c(ghg), get(str_c("data_EMS.M")), rowNames = TRUE)
-  addWorksheet(wb, str_c(ghg))
-  writeData(wb, str_c(ghg), get(str_c("data_EMS.CIM")), rowNames = TRUE)
-  
-  
-  }
-  # Adding the exports in monetary values
-  data_M <- readRDS(str_c(path_loader,"Imports.Y_",iso,".rds"))  %>%
-    select(-iso) %>% merge(sec.desc[[str_c(brd,".desc")]],., by = "products") %>%
-    select(-nProducts)
-  
-  # Writing the data into a excel workbook sheet
-  addWorksheet(wb, "IMPORTS")
-  writeData(wb, "IMPORTS", data_M, rowNames = TRUE)
-  
-  
-  # Exporting the results in the path_out folder
-  saveWorkbook(wb, file = str_c(path_user,path_export,"DATA_GHG_IMPORTED.xlsx"), overwrite = TRUE)
-  
-  
+  saveWorkbook(wb_dir, file = str_c(path_user,path_export,"DATA_GHG_DIR_IMPORTED.xlsx"), overwrite = TRUE)
