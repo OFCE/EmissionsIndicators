d56c2e0183010dd798c1f1b1f7563a99dd503f72
Paul Malliet <paul.malliet@sciencespo.fr>
2022-09-26 14:16:08 +0000

    add of argument from load.matrix function

--- src/01_exio3.Pre_loader.R
+++ src/01_exio3.Pre_loader.R
@@ -7,14 +7,17 @@ dir.create(str_c(path_out), recursive = TRUE)
 
 
 #X: total output
-load.matrix("x", year = 2015, export = TRUE)
+load.matrix("x", year = 2015, export = TRUE, row_index = "i.")
 
 #Z conso intérmediaires
-load.matrix("Z", year = 2015, export = TRUE)
+load.matrix("Z", year = 2015, export = TRUE, col_index = "i.", row_index = "p.")
+load.matrix("A", year = 2015, export = TRUE, col_index = "i.", row_index = "p.")
 
 ## Matrice Y (demande finale par pays et par type)
 #Z conso intérmediaires
-load.matrix("Y", year = 2015, export = TRUE)
+load.matrix("Y", year = 2015, export = TRUE,row_index = "p.")
 
 #F sattelite account 
-load.matrix("F", year = 2015, satellite = TRUE, export = TRUE)
+load.matrix("F", year = 2015, satellite = TRUE, export = TRUE, col_index = "i.")
+load.matrix("S", year = 2015, satellite = TRUE, export = TRUE, col_index = "i.")
+load.matrix("M", year = 2015, satellite = TRUE, export = TRUE, col_index = "i.")
