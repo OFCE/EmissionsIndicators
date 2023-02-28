4780ef8df75f276dc035e55ebc17393ebcb01677
PaulMalliet <paul.malliet@sciencespo.fr>
2023-02-28 09:54:28 +0100

    Update on products bridging

---
+++ data_in/desc/exio3_test.desc.R
@@ -0,0 +1,8 @@
+# Number of sectors and products in Exiobase 3
+exio3_test.desc <- list("nProducts" =    3,
+                        "products"  = c("Agg",
+                                        "Sea and coastal water transportation services",
+                                        "Inland water transportation services"),
+                        "codes"     = c("cagg",
+                                        "csea",
+                                        "cwat"))
